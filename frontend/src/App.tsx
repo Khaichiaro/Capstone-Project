7dc3708bf5f0da538e20b719b7a3a69c90454469
Sirionsr03 <152282163+Sirionsr03@users.noreply.github.com>
2025-05-22 21:18:59 +0700

    add file about foodrecord

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import './App.css'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import MenuRecHome from './page/MenuRecommand/home/index'
+import FoodRecordForm from './page/FoodRecordForm/foodrecordform'
+import FoodHistory from './page/FoodRecordForm/home'
+import EditFood from './page/FoodRecordForm/edit/editfoodform.tsx'
 
 function App() {
 
@@ -8,6 +11,9 @@ function App() {
    <Router>
         <Routes>
           <Route path="/" element={<MenuRecHome />} />
+          <Route path="/foodintake" element={<FoodRecordForm />} />
+          <Route path="/foodhistory" element={<FoodHistory />} />
+          <Route path="/editfood" element={<EditFood />} />
         </Routes>
     </Router>
     
